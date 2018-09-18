deb53c1cb7da810108478c756340cd09189f2dd1
Phillip Peschke <phillipdpeschke@gmail.com>
2018-09-18 13:37:56 -0500

    Adds chat to GameInputHandler
    - also prepares for game logic button presses
    
    	modified:   GameInputHandler.cpp

--- GameInputHandler.cpp
+++ GameInputHandler.cpp
@@ -1,5 +1,8 @@
 #include "GameInputHandler.h"
 #include "game.h"
+#include "NetworkClient.h"
+
+NetworkClient* getClient();
 
 GameInputHandler::GameInputHandler(Game* g, NetworkClient* c) : InputHandler(g, c)
 {
@@ -16,9 +19,55 @@ void GameInputHandler::handleMouseClick(InputEvent e)
 
 void GameInputHandler::handleButtonPress(InputEvent e)
 {
-	switch (e.getPressed())
+	if(thegame->typing)
 	{
-	case (char)27:
-		thegame->over = true;
+		switch(e.getPressed())
+		{
+		case (char)27:	//escape
+				thegame->typing = false;
+			break;
+		case (char)9:	//tab
+			thegame->typing = false;
+			break;
+		case (char)8:	//backspace
+			if(thegame->chatString.size() > 0)
+				thegame->chatString = thegame->chatString.substr(0, thegame->chatString.size() - 1);
+			break;
+		case (char)10:	//line feed
+		case (char)13:	//carriage return
+			getClient()->Send(thegame->chatString.c_str());
+			thegame->chatString = "";
+			thegame->typing = false;
+			break;
+		default:
+			if(thegame->typing)
+				thegame->chatString += e.getPressed();
+			break;
+		}
+	}
+	else
+	{
+		switch(e.getPressed())
+		{
+		case 't':
+		case 'T':
+			break;
+		case 'w':
+		case 'W':
+			break;
+		case 'b':
+		case 'B':
+			break;
+		case 'm':
+		case 'M':
+			break;
+		case 'g':
+		case 'G':
+			break;
+		case ' ':
+			break;
+		default:
+			break;
+		}
 	}
 }
